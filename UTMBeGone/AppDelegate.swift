a2642885891f0448867a22f130e888d3f96f09ae
Fernando Bunn <bunn@idevzilla.com>
2020-08-31 23:03:49 +0100

    Initial Commit

---
+++ UTMBeGone/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  UTMBeGone
+//
+//  Created by Fernando Bunn on 31/08/2020.
+//  Copyright © 2020 Fernando Bunn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
